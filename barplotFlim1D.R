a43a3d53cd0abb0ef007feb5c1e885cb2b7630f1
Cai Li <caili@ucdavis.edu>
2017-06-22 00:15:23 -0700

    barplotFlim1D & readMatFlimRoi1D
    Add Flim read 1D Roi data function
    Add Flim plot 1D Roi data function

---
+++ barplotFlim1D.R
@@ -0,0 +1,81 @@
+# barplotFlim1D is used for ploting the barplot of lifetime over different timepoints
+
+#    The function is written using ggplot, need to install ggplot2 package
+#    The function does not compare differnet groups at each time point
+
+#    inputData:  the output from readMatFlimRoi1D, the inputData should include the mean and 
+#    and std of the Flim variables like INT, LT, INTR; the odd index column is mean; the even
+#    index column is std
+#    timepoints:  a vector of time points used as x labels
+#    No_plots:  the No. of the plots going to be ploted
+#    NameOfSlectedField:  the plot name going to be ploted, like('meanINTR1')
+#    folderName:  the name of the folder to save the data if wanted
+#    imageSize:  the size of the image to be saved, a 2-elements vector, c(width, height)
+
+#    Copyright:2017-, Cai Li, UC Davis
+
+barplotFlim1D <- function(inputData, timepoints, nameOfXlabel, nameOfSlectedField, folderName,
+                          imageSize) {
+  
+  library(ggplot2)
+  
+  N_STATISTICS = 2
+  
+  # Determine which field to be ploted
+  
+  if (missing(nameOfSlectedField)) {
+    No_plots = seq(1, length(inputData), 2)
+  }
+  else {
+    No_plots = c(grep(nameOfSlectedField, colnames(inputData)))
+  }
+  
+  # Plot
+  
+  for (iField in No_plots) {
+    p = ggplot(data = inputData, aes(x = timepoints, y  = inputData[[iField]])) + 
+     geom_bar(stat = "identity") + 
+        
+     # add errorbar
+     geom_errorbar(aes(ymin = inputData[[iField]] - inputData[[iField + 1]],
+                       ymax = inputData[[iField]] + inputData[[iField + 1]]),
+                   width = 0.2, 
+                   position = position_dodge(0.9)) +
+        
+     # add labels
+     ylab(colnames(inputData)[iField]) +
+     xlab(nameOfXlabel) +
+        
+     # white background, add the axis lines
+     theme_classic() +
+        
+     # font size
+     theme(text = element_text(size = 20))
+    
+    # Save the data or Not
+    if(missing(imageSize)){
+      widthImage = 5
+      heightImage = 5
+    }
+    else {
+      widthImage = imageSize[1]
+      heightImage = imageSize[2]
+    }
+    
+    if(!missing(folderName)){
+      p = p + ggsave(paste0(folderName, '/',
+                           colnames(inputData)[iField], '.png'), 
+                     width = widthImage,
+                     height = heightImage)
+    }
+  }
+  
+  
+  # show the last image if missing nameOfSelectedField
+  # show the figure if having nameOfSelectedField
+  return(p)
+}
+
+# Example  
+# coltest = c("a", "b", "c", "d", "e");
+# barplotFlim1D(tes, coltest, 'time', folderName = '/Users/caili/Desktop/test')
---
+++ readMatFlimRoi1D.R
@@ -0,0 +1,65 @@
+# readMatFlimRoi1D is used for read all the mat files in a folder, each of which
+#   includes the mean and std for lifetime, intensity ratio and intensity; Then return
+#   a dataframe includes the 1D vectors of lifetime, intensity ratio and intensity over
+#   different files; 
+
+#   folderPath: Prefer absolute path
+
+#   Note: add "No_" to the head of the file name to determine the sequence
+#   Example: "1_20170621 SolvEffect BP_3h ROI"
+
+#   NaN will become 0
+#   Can be processed by other xxx1D functions
+
+#   Copyright, 2017-, Cai Li, UC Davis
+
+readMatFlimRoi1D = function(folderPath) {
+  
+  library(R.matlab)
+  
+  fileNamesInFolder = list.files(path = folderPath)
+  
+  N_FILES = length(fileNamesInFolder)
+  N_CHANNELS = 4
+  N_STATISTICS = 2
+  
+  colNames = c('meanINT1', 'stdINT1', 'meanINT2', 'stdINT2',
+               'meanINT3', 'stdINT3', 'meanINT4', 'stdINT4',
+               'meanLT1', 'stdLT1', 'meanLT2', 'stdLT2',
+               'meanLT3', 'stdLT3', 'meanLT4', 'stdLT4',
+               'meanINTR1', 'stdINTR1', 'meanINTR2', 'stdINTR2',
+               'meanINTR3', 'stdINTR3', 'meanINTR4', 'stdINTR4')
+  outputData = as.data.frame(
+    matrix(nrow = N_FILES, ncol = length(colNames),
+           dimnames = list(NULL, colNames)))
+  
+  for (iFile in 1:N_FILES) {
+    fileName = paste(folderPath, fileNamesInFolder[iFile], sep = '/')
+    fileData = readMat(fileName)
+    for (jVariable in 1:(length(fileData)/N_STATISTICS)) {
+      for (kChannel in 1:N_CHANNELS){
+        for (lStatistics in 1:N_STATISTICS) {
+          if (!is.na(fileData[[lStatistics +
+                              N_STATISTICS * (jVariable - 1)]][kChannel])) {
+            outputData[[lStatistics + 
+                         N_STATISTICS * (kChannel - 1) +
+                         N_CHANNELS * N_STATISTICS * (jVariable - 1)]][iFile] = 
+              fileData[[lStatistics +
+                         N_STATISTICS * (jVariable - 1)]][kChannel]
+          } # if the value is not NaN
+          else {
+            outputData[[lStatistics + 
+                          N_STATISTICS * (kChannel - 1) +
+                          N_CHANNELS * N_STATISTICS * (jVariable - 1)]][iFile] = 0
+          }
+        } # loop the statistics, like mean, std
+      } # loop the channels, like CH1, CH2
+    } # loop the variables, like INT, LT, INTR
+  } # loop the files in the folder
+  
+  return(outputData)
+  
+}
+
+# Example
+# tes = readMatFlimRoi1D('/Users/caili/Google Drive/Lab/Data/20170621 SolvEffect BP ROI Data')
